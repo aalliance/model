cbfb9ff2ee07f34fdf7c017880dbe1bbd0e513f7
Jordan T. Bates <jtbates@gmail.com>
2018-07-15 22:27:23 -0700

    use dirichlet boundary condition

--- dispersion/pde.m
+++ dispersion/pde.m
@@ -1,7 +1,8 @@
 %% Create Model and Geometry
 model = createpde();
-W = 10;
-R1 = [3;4;0;W;W;0;W;W;0;0];
+XW = 5;
+YW = 7;
+R1 = [3;4;0;XW;XW;0;YW;YW;0;0];
 gd = R1;
 sf = 'R1';
 ns = [82;49];
@@ -12,13 +13,13 @@ c = 0.05;
 specifyCoefficients(model,'m',0,'d',.1,'c',c,'a',0,'f',@fcoeffunction);
 
 %% Set Boundary Conditions
-applyBoundaryCondition(model,'neumann','Edge',1:4,'g',0,'q',1);
+applyBoundaryCondition(model,'dirichlet','Edge',1:4,'r',0,'h',1);
 
 %% Set Initial Conditions
 setInitialConditions(model,0);
 
 %% Generate and Plot Mesh
-msh = generateMesh(model,'Hmax',.15);
+msh = generateMesh(model,'Hmax',.2);
 %msh = generateMesh(model);
 figure;
 pdemesh(model); 
@@ -40,6 +41,5 @@ Umin = min(min(U));
 for j = 1:nframes
     pdeplot(model,'XYData',U(:,j));
     caxis([Umin Umax]);
-    %axis([-1 1 -1 1 0 1]);
     Mv(j) = getframe;
 end
