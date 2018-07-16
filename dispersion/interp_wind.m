af3813c2bdba35e39442c5bd88f32f5a74a120cc
Jordan T. Bates <jtbates@gmail.com>
2018-07-15 22:26:15 -0700

    Example for interpolating wind data

---
+++ dispersion/interp_wind.m
@@ -0,0 +1,40 @@
+w = csvread('wind.csv');
+u = unique(w(:, 1:2), 'rows'); % station locations
+ns = size(u,1);                % number of stations
+fits_sid_vx_t = cell(ns,1);
+fits_sid_vy_t = cell(ns,1);
+sdats = cell(ns,1);
+for sid = 1:ns
+    scoord = u(sid,:);
+    sdat = w(w(:,1)==scoord(1) & w(:,2)==scoord(2), :);
+    sdats{sid} = sdat;
+    fvxt = fit(sdat(:,3), sdat(:,4), 'pchipinterp');
+    fvyt = fit(sdat(:,3), sdat(:,5), 'pchipinterp');
+    fits_sid_vx_t{sid} = fvxt;
+    fits_sid_vy_t{sid} = fvyt;
+end
+%plot(fits_sid_vx_t{1}, sdats{1}(:,3), sdats{1}(:,4))
+
+t_begin = 1;
+t_end = 28;
+t_delta = 1;
+tvec = t_begin:t_delta:t_end;
+nt = length(tvec);
+fits_t_vx_xy = cell(nt,1);
+fits_t_vy_xy = cell(nt,1);
+for tq = tvec
+    sq = u;
+    for sid = 1:ns
+        fx = fits_sid_vx_t{sid};
+        sq(sid, 4) = feval(fx, tq);
+        fy = fits_sid_vx_t{sid};
+        sq(sid, 5) = feval(fy, tq);
+    end
+    fvxxy = fit(sq(:,1:2), sq(:,4), 'thinplateinterp');
+    fvyxy = fit(sq(:,1:2), sq(:,5), 'thinplateinterp');
+    fits_t_vx_xy{tq} = fvxxy;
+    fits_t_vy_xy{tq} = fvyxy;
+end
+%plot(fits_t_vx_xy{1});
+%[fx, fy] = differentiate(fits_t_vx_xy{1}, 200000, 200000)
+
