c998ebe2d21c0759aceb5ff49777a7c509533e05
Jordan T. Bates <jtbates@gmail.com>
2018-07-16 17:04:32 -0700

    Add wind divergence

--- dispersion2/fcoeffunction.m
+++ dispersion2/fcoeffunction.m
@@ -15,17 +15,16 @@ global t_begin;
         sigmay = 2000;
         A = 1;
         % Wind vector
-        [vx, vy, ~, ~] = windf(state.time, region.x, region.y);
+        [vx, vy, dxv, dyv] = windf(state.time, region.x, region.y);
         wind_advection = state.ux .* vx + state.uy .* vy;
-        % wind_divergence = ;
-        
+        wind_divergence = state.u .* (dxv + dyv);
         if state.time >= t_begin && state.time < train_end
             source = A*exp(-( (region.x-mux).^2 / (2*sigmax^2) + ...
                               (region.y-muy).^2 / (2*sigmay^2)));
         else
             source = 0;
         end
-        f(1,:) = source - wind_advection; % TODO add term for wind divergence
+        f(1,:) = source - wind_advection - wind_divergence;
     end
     ff = @fcfun;
 end
