c7077acd995d6fd41e9955cd251134ba6670ee1f
Jordan T. Bates <jtbates@gmail.com>
2018-07-19 00:01:34 -0700

    Use sft2m global in census.m

--- dispersion3/census.m
+++ dispersion3/census.m
@@ -1,12 +1,11 @@
+global ROI sft2m
 cdat = shaperead('ACS/ACS5Yr_BlockLevel');
 
 % filter out block groups that are not inside the region of interest
-mconv = 1200/3937; % 1 survey foot = 1200/3937 meters
-global ROI
 [roix, roiy] = boundary(ROI);
 inidx = [];
 for i = 1:length(cdat)
-    bgbb = cdat(i).BoundingBox*mconv; % block group bounding box
+    bgbb = cdat(i).BoundingBox*sft2m; % block group bounding box
     [bgbbx, bgbby] = boundary(bb2poly(bgbb));
     inroi = all(inpolygon(bgbbx, bgbby, roix, roiy));
     if inroi
