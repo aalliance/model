babc699edf94db0c341002cc17323ef0bfb4b74c
Jordan T. Bates <jtbates@gmail.com>
2018-07-19 00:03:32 -0700

    Use global start and end release locations

--- dispersion3/train.m
+++ dispersion3/train.m
@@ -1,9 +1,7 @@
 function mu = train(t)
-global t_begin train_end
+global t_begin train_end begin_r end_r
 period = train_end - t_begin;
-start_mu = [185319; 284379]; % 608k, 933k
-end_mu = [220980; 266701]; % 725k, 875k
 %dist_miles = sqrt(sum((start_mu - end_mu).^2))*0.000621371
-mu = start_mu + (end_mu - start_mu)/period*(t-t_begin);
+mu = begin_r + (end_r - begin_r)/period*(t-t_begin);
 
 
